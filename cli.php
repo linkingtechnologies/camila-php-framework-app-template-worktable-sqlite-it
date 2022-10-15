349a59b6dd46e29948ec1550ab3b96620db89845
umbi <umbi@linkingtechnologies.it>
2022-10-16 00:05:12 +0200

    Update cli.php
    cli.php updated for plugin installation

--- cli.php
+++ cli.php
@@ -13,6 +13,7 @@ require('../../camila/config.inc.php');
 require('../../camila/i18n.inc.php');
 require('../../camila/camila_hawhaw.php');
 require('../../camila/database.inc.php');
+require('../../camila/plugins.class.inc.php');
 
 class CamilaAppCli extends CLI
 {
@@ -21,6 +22,10 @@ class CamilaAppCli extends CLI
     {
 		$options->registerCommand('init-app', 'Create new App');
         $options->registerArgument('lang', 'App language', true, 'init-app');
+		
+		$options->registerCommand('install-plugin', 'Install plugin');
+        $options->registerArgument('name', 'Plugin name', true, 'install-plugin');
+		$options->registerArgument('lang', 'Plugin language', true, 'install-plugin');
     }
 
     protected function main(Options $options)
@@ -28,6 +33,9 @@ class CamilaAppCli extends CLI
         switch ($options->getCmd()) {
             case 'init-app':
 				$this->initApp($options);
+                break;
+			case 'install-plugin':
+				$this->installPlugin($options);
                 break;
             default:
                 $this->error('No known command was called, we show the default help instead:');
@@ -43,6 +51,29 @@ class CamilaAppCli extends CLI
 		$camilaApp->db = $db;
 		$camilaApp->lang = $lang;
 		$camilaApp->resetTables(CAMILA_TABLES_DIR);	
+	}
+	
+	protected function installPlugin(Options $options) {
+		$name = $options->getArgs()[0];
+		$lang = $options->getArgs()[1];
+		if (is_dir('plugins/'.$name)) {
+			$this->error('Plugin already in use!');
+		} else {
+			$zipFile = bin2hex(random_bytes(10)).'.zip';
+			file_put_contents('plugins/'.$zipFile, file_get_contents('https://github.com/linkingtechnologies/camila-php-framework-app-plugin-'.$name.'/archive/refs/heads/main.zip'));
+			$zip = new ZipArchive;
+			if ($zip->open('plugins/'.$zipFile) === TRUE) {
+				$zip->extractTo('plugins/');
+				$zip->close();
+				rename('plugins/camila-php-framework-app-plugin-'.$name.'-main', 'plugins/'.$name);
+				unlink('plugins/'.$zipFile);
+				global $_CAMILA;
+				CamilaPlugins::install($_CAMILA['db'], $lang, $name);
+				$this->success('Plugin ' . $options->getArgs()[0] . ' installed!');
+			} else {
+				$this->error('Error extracting template zip file');
+			}
+		}
 	}
 }
 
