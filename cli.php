666a488543d88ed8b852cc7e467e6113f4d4a27e
umbi <umbi@linkingtechnologies.it>
2022-10-30 14:02:25 +0100

    Update cli.php

--- cli.php
+++ cli.php
@@ -1,8 +1,17 @@
 #!/usr/bin/php
 <?php
 
-use splitbrain\phpcli\CLI;
-use splitbrain\phpcli\Options;
+require '../../vendor/autoload.php';
+
+
+require_once('../../camila/autoloader.inc.php');
+
+require('../../camila/config.inc.php');
+
+require('../../camila/i18n.inc.php');
+require('../../camila/camila_hawhaw.php');
+require('../../camila/database.inc.php');
+require('../../camila/plugins.class.inc.php');
 
 require CAMILA_DIR. 'cli/Exception.php';
 require CAMILA_DIR. 'cli/TableFormatter.php';
@@ -10,6 +19,11 @@ require CAMILA_DIR. 'cli/Options.php';
 require CAMILA_DIR. 'cli/Base.php';
 require CAMILA_DIR. 'cli/Colors.php';
 require CAMILA_DIR. 'cli/CLI.php';
+
+use splitbrain\phpcli\CLI;
+use splitbrain\phpcli\Options;
+use splitbrain\phpcli\Exception;
+
 
 class CamilaAppCli extends CLI
 {
